59d69a770ec793d5643fab718462fe525afd2a35
Fabian Fett <fabianfett@mac.com>
2020-02-27 18:19:43 +0100

    Added more parsing tests (#6)

--- Tests/JSONParsingTests/ArrayParserTests.swift
+++ Tests/JSONParsingTests/ArrayParserTests.swift
@@ -19,6 +19,31 @@ class ArrayParserTests: XCTestCase {
     XCTAssertEqual(result, [.bool(true)])
   }
   
+  func testNestedArrays() throws {
+    var parser = JSONParserImpl(bytes: [UInt8]("[[]]".utf8))
+    let _ = try XCTUnwrap(parser.reader.read())
+   
+    let result = try parser.parseArray()
+    XCTAssertEqual(result, [.array([])])
+  }
+  
+  func testHighlyNestedArray() throws {
+    // test 512 should succeed
+    let passingString = String(repeating: "[", count: 512) +  String(repeating: "]", count: 512)
+    _  = try JSONParser().parse(bytes: [UInt8](passingString.utf8))
+    
+    let failingString = String(repeating: "[", count: 513)
+    do {
+      _  = try JSONParser().parse(bytes: [UInt8](failingString.utf8))
+    }
+    catch JSONError.tooManyNestedArraysOrDictionaries(characterIndex: 512) {
+      //expected case
+    }
+    catch {
+      XCTFail("Unexpected error: \(error)")
+    }
+  }
+  
   func testSimpleTrueAndStringArray() throws {
     var parser = JSONParserImpl(bytes: [UInt8](#"[   true  , "hello" ]"#.utf8))
     let _ = try XCTUnwrap(parser.reader.read())
@@ -27,7 +52,7 @@ class ArrayParserTests: XCTestCase {
     XCTAssertEqual(result, [.bool(true), .string("hello")])
   }
   
-  func testCommaBeforeEnd() throws {
+  func testCommaBeforeEnd() {
     do {
       var parser = JSONParserImpl(bytes: [UInt8]("[   true  , ]".utf8))
       _ = try XCTUnwrap(parser.reader.read())
@@ -42,7 +67,7 @@ class ArrayParserTests: XCTestCase {
     }
   }
   
-  func testInvalidCharacterAfterFirstValue() throws {
+  func testInvalidCharacterAfterFirstValue() {
     do {
       var parser = JSONParserImpl(bytes: [UInt8]("[ true asd ]".utf8))
       _ = try XCTUnwrap(parser.reader.read())
@@ -57,17 +82,28 @@ class ArrayParserTests: XCTestCase {
     }
   }
   
-  func testHighlyNestedArray() throws {
-    // test 512 should succeed
-    let passingString = String(repeating: "[", count: 512) +  String(repeating: "]", count: 512)
-    _  = try JSONParser().parse(bytes: [UInt8](passingString.utf8))
-    
-    let failingString = String(repeating: "[", count: 513)
+  func testNumberCommaBeforeEnd() {
     do {
-      _  = try JSONParser().parse(bytes: [UInt8](failingString.utf8))
+      var parser = JSONParserImpl(bytes: [UInt8]("[   1  , ]".utf8))
+      _ = try XCTUnwrap(parser.reader.read())
+      _ = try parser.parseArray()
+      XCTFail("this point should not be reached")
     }
-    catch JSONError.tooManyNestedArraysOrDictionaries(characterIndex: 512) {
-      //expected case
+    catch JSONError.unexpectedCharacter(ascii: UInt8(ascii: "]")) {
+      // expected
+    }
+    catch {
+      XCTFail("Unexpected error: \(error)")
+    }
+  }
+  
+  func testIncompleteLiteralInArray() throws {
+    do {
+      _ = try JSONParser().parse(bytes: [UInt8]("[ nu ]".utf8))
+      XCTFail("this point should not be reached")
+    }
+    catch JSONError.unexpectedCharacter(ascii: UInt8(ascii: " ")) {
+      // expected
     }
     catch {
       XCTFail("Unexpected error: \(error)")
--- Tests/JSONParsingTests/ObjectParserTests.swift
+++ Tests/JSONParsingTests/ObjectParserTests.swift
@@ -44,4 +44,25 @@ class ObjectParserTests: XCTestCase {
       XCTFail("Unexpected error: \(error)")
     }
   }
+  
+  func testEmptyKey() throws {
+    var parser = JSONParserImpl(bytes: [UInt8]("{\"\": \"foobar\"}".utf8))
+    let _ = try XCTUnwrap(parser.reader.read())
+    
+    let result = try parser.parseObject()
+    XCTAssertEqual(result, ["": .string("foobar")])
+  }
+  
+  func testDuplicateKey() throws {
+    var parser = JSONParserImpl(bytes: [UInt8]("{\"a\": \"foo\", \"a\": \"bar\"}".utf8))
+    let _ = try XCTUnwrap(parser.reader.read())
+    
+    // current behavior is last key is given, should be documented?
+    // rfc does not define how to handle duplicate keys
+    // parser should be able to parse anyways
+    
+    let result = try parser.parseObject()
+    XCTAssertEqual(result, ["a": .string("bar")])
+  }
+
 }
