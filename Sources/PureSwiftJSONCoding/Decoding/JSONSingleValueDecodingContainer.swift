134d663d8f622589eb1145ff9d0ddfbda97fab8d
Fabian Fett <fabianfett@mac.com>
2020-03-19 14:22:47 +0100

    Fix typo decodeFixedWithInteger() -> decodeFixedWidthInteger()
    - Fix issue #18

--- Sources/PureSwiftJSONCoding/Decoding/JSONSingleValueDecodingContainer.swift
+++ Sources/PureSwiftJSONCoding/Decoding/JSONSingleValueDecodingContainer.swift
@@ -41,43 +41,43 @@ struct JSONSingleValueDecodingContainter: SingleValueDecodingContainer {
   }
   
   func decode(_ type: Int.Type) throws -> Int {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: Int8.Type) throws -> Int8 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: Int16.Type) throws -> Int16 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: Int32.Type) throws -> Int32 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: Int64.Type) throws -> Int64 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: UInt.Type) throws -> UInt {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: UInt8.Type) throws -> UInt8 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: UInt16.Type) throws -> UInt16 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: UInt32.Type) throws -> UInt32 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode(_ type: UInt64.Type) throws -> UInt64 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
@@ -93,7 +93,7 @@ extension JSONSingleValueDecodingContainter {
       debugDescription: "Expected to decode \(type) but found \(value.debugDataTypeDescription) instead."))
   }
   
-  @inline(__always) private func decodeFixedWithInteger<T: FixedWidthInteger>() throws
+  @inline(__always) private func decodeFixedWidthInteger<T: FixedWidthInteger>() throws
     -> T
   {
     guard case .number(let number) = value else {
--- Sources/PureSwiftJSONCoding/Decoding/JSONUnkeyedDecodingContainer.swift
+++ Sources/PureSwiftJSONCoding/Decoding/JSONUnkeyedDecodingContainer.swift
@@ -72,43 +72,43 @@ struct JSONUnkeyedDecodingContainer: UnkeyedDecodingContainer {
   }
   
   mutating func decode(_ type: Int.Type) throws -> Int {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: Int8.Type) throws -> Int8 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: Int16.Type) throws -> Int16 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: Int32.Type) throws -> Int32 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: Int64.Type) throws -> Int64 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: UInt.Type) throws -> UInt {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: UInt8.Type) throws -> UInt8 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: UInt16.Type) throws -> UInt16 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: UInt32.Type) throws -> UInt32 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode(_ type: UInt64.Type) throws -> UInt64 {
-    return try self.decodeFixedWithInteger()
+    return try self.decodeFixedWidthInteger()
   }
   
   mutating func decode<T>(_ type: T.Type) throws -> T where T : Decodable {
@@ -149,7 +149,7 @@ extension JSONUnkeyedDecodingContainer {
   }
 
   
-  @inline(__always) private mutating func decodeFixedWithInteger<T: FixedWidthInteger>() throws
+  @inline(__always) private mutating func decodeFixedWidthInteger<T: FixedWidthInteger>() throws
     -> T
   {
     defer {
