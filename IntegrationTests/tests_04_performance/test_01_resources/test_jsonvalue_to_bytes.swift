9ef067bbc5832d383e74a4bfb600f1083e4ff378
Fabian Fett <fabianfett@mac.com>
2020-03-21 14:19:00 +0100

    Stricter Allocation Test
    thx for making everything a little stricter @weissi

--- IntegrationTests/tests_04_performance/test_01_resources/test_jsonvalue_to_bytes.swift
+++ IntegrationTests/tests_04_performance/test_01_resources/test_jsonvalue_to_bytes.swift
@@ -1,17 +1,19 @@
 import PureSwiftJSONParsing
 
 func run(identifier: String) {
-    let sampleString = SampleStructure.sampleJSON
-    let sampleBytes  = [UInt8](sampleString.utf8)
-    let sampleJSON   = try! JSONParser().parse(bytes: sampleBytes)
-    
-    measure(identifier: identifier) {
-        for _ in 0..<1_000 {
-          var bytes = [UInt8]()       // <-- this is the only real allocation
-          bytes.reserveCapacity(6000) // <-- reserve capactity so high we don't need reallocs
-          sampleJSON.appendBytes(to: &bytes)
-        }
-      
-        return 1_000
+  let sampleString = SampleStructure.sampleJSON
+  let sampleBytes  = [UInt8](sampleString.utf8)
+  let sampleJSON   = try! JSONParser().parse(bytes: sampleBytes)
+
+  var bytes = [UInt8]()       // <-- this is the only real allocation
+  bytes.reserveCapacity(6000) // <-- reserve capactity so high we don't need reallocs
+  
+  measure(identifier: identifier) {
+    for _ in 0..<1_000 {
+      bytes.removeAll(keepingCapacity: true)
+      sampleJSON.appendBytes(to: &bytes)
     }
+  
+    return 1_000
+  }
 }
